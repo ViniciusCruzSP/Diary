cfd819e479d1f7b65314e9531528f395fadbcd0e
Vinícius da Cruz <vinicius.costa@bib.com.br>
2025-12-15 09:52:14 -0300

    feat: front-end of Create page

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -21,5 +21,10 @@ namespace DiaryApp.Controllers
         {
             return View();
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
