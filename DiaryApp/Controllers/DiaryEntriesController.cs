4a8ad6a166946664274761395451f5e80484f7f1
Vinícius da Cruz <vinicius.costa@bib.com.br>
2025-12-23 12:48:37 -0300

    feat: Edit post method complete

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -42,5 +42,34 @@ namespace DiaryApp.Controllers
             }
             return View(obj);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(DiaryEntry obj)
+        {
+            if (obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "Title too short");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Diary");
+            }
+            return View(obj);
+        }
     }
 }
