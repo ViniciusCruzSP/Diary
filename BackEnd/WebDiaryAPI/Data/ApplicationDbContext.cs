e64b7b01540121e2b3e088615c43ba9d6c0c2fd4
Vinícius da Cruz <vinicius.costa@bib.com.br>
2026-01-28 11:08:10 -0300

    feat(api): integrate Identity into ApplicationDbContext

--- BackEnd/WebDiaryAPI/Data/ApplicationDbContext.cs
+++ BackEnd/WebDiaryAPI/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebDiaryAPI.Models.Identity;
 using WebDiaryAPI.Models;
 
 namespace WebDiaryAPI.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
